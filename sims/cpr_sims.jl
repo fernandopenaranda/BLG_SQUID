a51d97a46020524626b49b14f92ccf8f5e8997e1
fernandopenaranda <fernando.pennaranda@gmail>
2022-02-08 18:00:36 +0100

    full spectrum

--- sims/cpr_sims.jl
+++ sims/cpr_sims.jl
@@ -1,9 +1,9 @@
 using StaticArrays
 
 p = Params(Ln = 200, W = 200, Ls = 20, Ws = 20, scale = 40, λ = 5,
-          Δ = 1, d = 0, τ = 1, B = SA[0,0,2*fΦ], EZ = SA[0,0,0], μN = 1e-6);
-fΦ = 0.01
-Blist = collect(0.:fΦ/5:2*fΦ)
+          Δ = 1, d = 0, τ = 1, B = SA[0,0,fΦ], EZ = SA[0,0,0], μN = 1e-6);
+fΦ = 0.05
+Blist = collect(0.:fΦ/6:10*fΦ)
 ic = maxicϕ_exactdiag(Blist, p)
 # ph = rectangle_weaklink(p, false);
 ##############################
